3a621db548b8149533a9d30d9404d7f869d3729d
unknown <vitaly.sadikov1@yandex.ru>
2025-09-25 16:16:41 +0500

    fix: fixed dump.sql

--- dumps/dump.sql
+++ dumps/dump.sql
@@ -2,7 +2,6 @@
 -- PostgreSQL database dump
 --
 
-\restrict LeRReGudfqECZxFH83bRQ9byuFLFOkM0CdDPK2HJ5Lxi1IN2BMGHHyKjv7hczVz
 
 -- Dumped from database version 17.6
 -- Dumped by pg_dump version 17.6
@@ -10,7 +9,6 @@
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
-SET transaction_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
 SELECT pg_catalog.set_config('search_path', '', false);
@@ -1173,5 +1171,4 @@ REVOKE USAGE ON SCHEMA public FROM PUBLIC;
 -- PostgreSQL database dump complete
 --
 
-\unrestrict LeRReGudfqECZxFH83bRQ9byuFLFOkM0CdDPK2HJ5Lxi1IN2BMGHHyKjv7hczVz
 
